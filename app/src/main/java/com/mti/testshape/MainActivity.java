a6109484722be7fe13bc729265a4695341661cd5
Tareq3 <mti.tareq3@gmail.com>
2018-10-18 15:44:37 +0600

    Initial commit

---
+++ app/src/main/java/com/mti/testshape/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Tareq Islam on 10/14/18 11:00 PM
+ *
+ *  Last modified 10/14/18 7:47 PM
+ */
+
+package com.mti.testshape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
